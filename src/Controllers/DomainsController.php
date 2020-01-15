cdb2d5c21c5ad9de5234f9af501c1a4999fee50d
Ilia Yatsenko <ilia@mpisoft.com>
2020-01-15 19:50:09 +0600

    test

--- src/Controllers/DomainsController.php
+++ src/Controllers/DomainsController.php
@@ -4,6 +4,10 @@ namespace App\Controllers;
 
 use App\Controllers\Dtos\DomainDto;
 use App\Controllers\Params\GetDomainPricesParams;
+use App\Domains\Repositories\DomainRepository;
+use App\Domains\Repositories\TldRepository;
+use App\Utils\Domains;
+use App\Utils\ObjectArrays;
 use Yii;
 use yii\filters\ContentNegotiator;
 use yii\web\BadRequestHttpException;
@@ -40,14 +44,39 @@ class DomainsController extends Controller
             throw new BadRequestHttpException();
         }
 
-        // todo найти список tld из таблицы
-        // todo создать список доменов
-        // todo проверить домены на корректность имени
-        // todo проверить наличие домена в таблице domain
-        // todo создать список dto с ценами для списка доменов
 
+        // get container EM
+        $em = Yii::$container->get('Doctrine\ORM\EntityManager');
+
+        // todo найти список tld из таблицы
+        $tr = new TldRepository($em);
+        $tlds = $tr->findAll();
+
+        // todo создать список доменов
+        $domains = Domains::fromNameAndTlds($params['search'], ObjectArrays::createFieldArray($tlds, 'tld'));
+
+        // todo проверить домены на корректность имени
+        $domains = array_filter($domains, ["App\Utils\Domains", "valid"]);
+
+        // todo проверить наличие домена в таблице domain
+        $dr = new DomainRepository($em);
+        $domains = array_filter($domains, function($item) use ($dr) {
+            if($dr->findOneByDomain($item) !== null) {
+                return false;
+            }
+            return true;
+        });
+
+        // todo создать список dto с ценами для списка доменов
         /** @var DomainDto[] $dtos */
         $dtos = [];
+        foreach($domains as $domain) {
+            preg_match('/\.(\w+)$/', $domain, $matches);
+//            VarDumper::dump($tld, 100, true);exit();
+            $tldObject = ObjectArrays::filterEqualOne($tlds, 'tld', $matches[1]);
+            $dtos[] = new DomainDto($matches[1], $domain, $tldObject->price, true);
+        };
+
         return $dtos;
     }
 }
